79112635a08e8893d98730991151608332a8b3e0
Ryan Cirincione <ryancirincione@aol.com>
2018-04-21 12:12:19 -0400

    Basics to Vertex

--- src/Vertex.java
+++ src/Vertex.java
@@ -3,6 +3,12 @@ public class Vertex {
 	public double x, y, heat;
 	
 	public Vertex(double x, double y) {
-		
+		this(x, y, 0);
+	}
+	
+	public Vertex(double x, double y, double heat) {
+		this.x = x;
+		this.y = y;
+		this.heat = heat;
 	}
 }
