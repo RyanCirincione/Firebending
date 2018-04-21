75f4197bdb89b64ee696bede60ba6c5261638370
Andrew Cater <acater@gmail.com>
2018-04-21 12:39:20 -0400

    started fireGenerator Class

---
+++ src/Firegenerator.java
@@ -0,0 +1,19 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+public class Firegenerator 
+{
+	public void paintFire(Graphics video, ArrayList<Vertex> vertices)//verices going clockwise
+	{
+		
+		for(Vertex current : vertices)
+		{
+			
+		}
+		
+	}
+	public Vertex findCenter (ArrayList<Vertex> vertices)
+	{
+		return null;
+	}
+}
